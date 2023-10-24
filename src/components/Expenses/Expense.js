ebdbfd8c801c9c878721405e4ebaeb7fd105f6a5
Prem <premmittal88@gmail.com>
2023-10-24 17:09:14 +0530

    Project completed

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -12,9 +12,9 @@ function Expense(props){
     <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </div>
     </Card>
   );
